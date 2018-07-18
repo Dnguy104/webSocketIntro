8ad607075da59595cad51177bf26851c2c18382e
Dnguy104 <dnguy104@ucr.edu>
2018-07-18 14:38:19 -0700

    removing jquery

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
-$(function () {
-    "use strict";
+"use strict";
+
+window.onload = function() {
 
     // for better performance - to avoid searching in DOM
-    var content = $('#content');
-    var input = $('#input');
-    var status = $('#status');
+    var content = document.getElementById('content');
+    var input = document.getElementById('input');
+    var status = document.getElementById('status');
 
     // my color assigned by the server
     var myColor = false;
@@ -16,10 +17,9 @@ $(function () {
 
     // if browser doesn't support WebSocket, just show some notification and exit
     if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
-                                    + 'support WebSockets.'} ));
-        input.hide();
-        $('span').hide();
+        content.innerHTML = '<p>Sorry, but your browser doesn\'t  support WebSockets.</p>';
+        input.style.display = 'none';
+        document.querySelector('span').style.display = 'none';
         return;
     }
 
@@ -28,21 +28,20 @@ $(function () {
 
     connection.onopen = function () {
         // first we want users to enter their names
-        input.removeAttr('disabled');
-        status.text('Choose name:');
+        input.disabled = false;
+        var txt = document.createTextNode('Choose name:');
+        status.removeChild(status.firstChild);
+        status.appendChild(txt);
     };
 
     connection.onerror = function (error) {
         // just in there were some problems with conenction...
-        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
-                                    + 'connection or the server is down.' } ));
+        content.innerHTML = 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.';
     };
 
-    // most important part - incoming messages
     connection.onmessage = function (message) {
-        // try to parse JSON message. Because we know that the server always returns
-        // JSON this should work without any problem but we should make sure that
-        // the massage is not chunked or otherwise damaged.
+
         try {
             var json = JSON.parse(message.data);
         } catch (e) {
@@ -50,21 +49,23 @@ $(function () {
             return;
         }
 
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
         if (json.type === 'color') { // first response from the server with user's color
             myColor = json.data;
-            status.text(myName + ': ').css('color', myColor);
-            input.removeAttr('disabled').focus();
-            // from now user can start sending messages
+            var txt = document.createTextNode(myName + ': ');
+            status.removeChild(status.firstChild);
+            status.appendChild(txt);
+            status.style.color = myColor
+            input.disabled = false;
+            input.focus();
+
         } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
             for (var i=0; i < json.data.length; i++) {
                 addMessage(json.data[i].author, json.data[i].text,
                            json.data[i].color, new Date(json.data[i].time));
             }
+            
         } else if (json.type === 'message') { // it's a single message
-            input.removeAttr('disabled'); // let the user write another message
+            input.disabled = false;  // let the user write another message
             addMessage(json.data.author, json.data.text,
                        json.data.color, new Date(json.data.time));
         } else {
@@ -75,18 +76,18 @@ $(function () {
     /**
      * Send mesage when user presses Enter key
      */
-    input.keydown(function(e) {
+    input.addEventListener("keydown", function(e) {
         if (e.keyCode === 13) {
-            var msg = $(this).val();
+            var msg = input.value;
             if (!msg) {
                 return;
             }
             // send the message as an ordinary text
             connection.send(msg);
-            $(this).val('');
+            input.value = '';
             // disable the input field to make the user wait until server
             // sends back response
-            input.attr('disabled', 'disabled');
+            input.disabled = true;
 
             // we know that the first message sent from a user their name
             if (myName === false) {
@@ -102,9 +103,11 @@ $(function () {
      */
     setInterval(function() {
         if (connection.readyState !== 1) {
-            status.text('Error');
-            input.attr('disabled', 'disabled').val('Unable to comminucate '
-                                                 + 'with the WebSocket server.');
+            var txt = document.createTextNode('Error');
+            status.removeChild(status.firstChild);
+            status.appendChild(txt);
+            input.disabled = true;
+            input.value = 'Unable to comminucate with the WebSocket server.';
         }
     }, 3000);
 
@@ -112,9 +115,10 @@ $(function () {
      * Add message to the chat window
      */
     function addMessage(author, message, color, dt) {
-        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+        content.innerHTML += '<p><span style="color:' + color + '">' + author + '</span> @ ' +
              + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
              + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-             + ': ' + message + '</p>');
-    }
-});
+             + ': ' + message + '</p>';
+    };
+    
+};
